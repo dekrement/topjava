f83eeb68bd326e8cbd16447bc65c2787614b8937
web <dekrement@gmail.com>
2017-05-15 10:39:50 +0300

    HW6 begin

---
+++ src/main/java/ru/javawebinar/topjava/repository/CacheUtil.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.topjava.repository;
+
+/**
+ * Created by web on 10.05.2017.
+ */
+public class CacheUtil {
+    public void evictCache() {
+        try {
+
+        } catch(Exception e) {
+
+        }
+    }
+}
